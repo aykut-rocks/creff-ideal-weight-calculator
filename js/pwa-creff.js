ebe15fd52972c10f9a455af153f3cae8f77fed84
Sensoy, Aykut <aykut.sensoy@bild.de>
2018-01-05 14:40:32 +0100

    added basic layout and functionality

---
+++ js/pwa-creff.js
@@ -0,0 +1,16 @@
+var calculateIdealWeight = () => {
+
+	const personHeight = parseInt(document.querySelector('#height').value, 10);
+	const personAge = parseInt(document.querySelector('#age').value, 10);
+	const personSlimness = document.querySelector('#slimness');
+	const personHeightFactor = personHeight - 100;
+	const personAgeFactor = personAge/10;
+	const personSlimnessFactor = parseFloat(personSlimness.options[personSlimness.selectedIndex].value, 10);
+
+	let idealWeight = (personHeightFactor + personAgeFactor) * 0.9 * personSlimnessFactor;
+	let idealWeightContainer = document.querySelector('.ideal').innerHTML = idealWeight.toFixed(2);
+};
+
+var resetForm = () => {
+	document.querySelector('#creff').reset();
+}
