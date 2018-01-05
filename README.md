df9ba2e95e776d999dcaf96e12d5a6367de49f4a
Sensoy, Aykut <aykut.sensoy@bild.de>
2018-01-05 15:40:08 +0100

    minor optimizations

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# pwa-creff
-calculate a persons ideal weight based on the CREFF formula
+# creff ideal weight calculator
+calculates a persons ideal weight based on the CREFF formula
--- js/creff-calculator.js
+++ js/creff-calculator.js
@@ -3,14 +3,17 @@ var calculateIdealWeight = () => {
 	const personHeight = parseInt(document.querySelector('#height').value, 10);
 	const personAge = parseInt(document.querySelector('#age').value, 10);
 	const personSlimness = document.querySelector('#slimness');
+
 	const personHeightFactor = personHeight - 100;
 	const personAgeFactor = personAge/10;
 	const personSlimnessFactor = parseFloat(personSlimness.options[personSlimness.selectedIndex].value, 10);
 
 	let idealWeight = (personHeightFactor + personAgeFactor) * 0.9 * personSlimnessFactor;
-	let idealWeightContainer = document.querySelector('.ideal').innerHTML = idealWeight.toFixed(2);
+	let idealWeightContainer = document.querySelector('.ideal-weight');
+
+	idealWeightContainer.innerHTML = idealWeight.toFixed(2);
 };
 
-var resetForm = () => {
-	document.querySelector('#creff').reset();
-}
+var resetCreffForm = () => {
+	document.querySelector('#creff-form').reset();
+};
