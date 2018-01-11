62b8a7711e9f8da91042ec3de1d5ebbe527a5f98
Sensoy, Aykut <aykut.sensoy@bild.de>
2018-01-11 09:14:50 +0100

    added basic form validation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # creff ideal weight calculator
 calculates a persons ideal weight based on the CREFF formula
+
+## constraints
+- expects a minimum height of 125cm
+- expects a minimum age of 5
--- js/creff-calculator.js
+++ js/creff-calculator.js
@@ -1,19 +1,32 @@
+const creffForm = document.getElementById('creff-form');
+const heightCreffForm = document.getElementById('height');
+const ageCreffForm = document.getElementById('age');
+const idealWeightContainer = document.querySelector('.ideal-weight');
+
 var calculateIdealWeight = () => {
+	const personHeight = parseInt(heightCreffForm.value, 10);
+	const personAge = parseInt(ageCreffForm.value, 10);
+	const personSlimness = document.getElementById('slimness');
 
-	const personHeight = parseInt(document.querySelector('#height').value, 10);
-	const personAge = parseInt(document.querySelector('#age').value, 10);
-	const personSlimness = document.querySelector('#slimness');
-
-	const personHeightFactor = personHeight - 100;
-	const personAgeFactor = personAge/10;
-	const personSlimnessFactor = parseFloat(personSlimness.options[personSlimness.selectedIndex].value, 10);
+	let personHeightFactor = personHeight - 100;
+	let personAgeFactor = personAge/10;
+	let personSlimnessFactor = parseFloat(personSlimness.options[personSlimness.selectedIndex].value, 10);
 
 	let idealWeight = (personHeightFactor + personAgeFactor) * 0.9 * personSlimnessFactor;
-	let idealWeightContainer = document.querySelector('.ideal-weight');
 
 	idealWeightContainer.innerHTML = idealWeight.toFixed(2);
 };
 
 var resetCreffForm = () => {
-	document.querySelector('#creff-form').reset();
+	creffForm.reset();
 };
+
+creffForm.addEventListener("submit", function(event) {
+	let heightError = heightCreffForm.validity.patternMismatch;
+	let ageError =  ageCreffForm.validity.patternMismatch;
+	
+	if(!heightError && !ageError) {
+		calculateIdealWeight();
+		event.preventDefault();
+	}
+}, false);
